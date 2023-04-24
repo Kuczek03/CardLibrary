7fc67ef65c3964408cfde775de4b175cb3ffb712
TheKolorowy <thekolorowyyt@gmail.com>
2023-04-24 09:46:57 +0200

    Project

---
+++ src/main/java/pl/cardlibrary/CardLibrary/CardLibraryApplication.java
@@ -0,0 +1,13 @@
+package pl.cardlibrary.CardLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/cardlibrary/CardLibrary/CardLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.cardlibrary.CardLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
